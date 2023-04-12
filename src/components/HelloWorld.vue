5e52e5c6fda68a98150dc03bc93b5fdb54f594b3
Eric G <eric.gorr@twosixtech.com>
2023-04-12 09:18:15 -0400

    [main] update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,26 +1,17 @@
 <template>
-  <div class="hello">
+  <div ref="cesiumContainer" id="cesiumContainer">
     hello
   </div>
 </template>
 
 <script setup>
+import { Viewer } from "cesium";
+import "cesium/Build/Cesium/Widgets/widgets.css";
+
+const viewer = new Viewer("cesiumContainer");
+
+console.log( "viewer", viewer)
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 </style>
